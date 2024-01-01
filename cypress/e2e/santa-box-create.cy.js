64c501540f9941daa5063cf7a0cb7f5e26e853e3
Anastasiia Morozova <nmorozova283@gmail.com>
2024-01-01 20:41:53 +0200

    hw 6.1.2 deleting box via API

--- cypress/e2e/santa-box-create.cy.js
+++ cypress/e2e/santa-box-create.cy.js
@@ -24,12 +24,18 @@ describe("user can create a box and run it", () => {
   let maxAmount = 50;
   let currency = "Евро";
   let inviteLink;
+  let boxKey;
 
   it("user logins and create a box", () => {
     cy.visit("/login");
     cy.login(users[0].email, users[0].password);
     cy.get('.home-page-buttons > [href="/box/new"] > .btn-main').click();
     cy.get(boxPage.boxNameField).type(newBoxName);
+    cy.get(boxPage.idField)
+      .invoke('val')
+      .then((value) => {
+        boxKey = value;
+      });
     cy.get(generalElements.arrowRight).click();
     cy.get(boxPage.sixthIcon).click();
     cy.get(generalElements.arrowRight).click();
@@ -70,18 +76,7 @@ describe("user can create a box and run it", () => {
 
   after("delete box", () => {
     cy.visit("/login");
-    cy.login(users[0].email, users[0].password);
-    cy.get(
-      '.layout-1__header-wrapper-fixed > .layout-1__header > .header > .header__items > .layout-row-start > [href="/account/boxes"] > .header-item > .header-item__text > .txt--med'
-    ).click();
-    cy.get(":nth-child(1) > a.base--clickable > .user-card").first().click();
-    cy.get(
-      ".layout-1__header-wrapper-fixed > .layout-1__header-secondary > .header-secondary > .header-secondary__right-item > .toggle-menu-wrapper > .toggle-menu-button > .toggle-menu-button--inner"
-    ).click();
-    cy.contains("Архивация и удаление").click({ force: true });
-    cy.get(":nth-child(2) > .form-page-group__main > .frm-wrapper > .frm").type(
-      "Удалить коробку"
-    );
-    cy.get(".btn-service").click();
+    cy.login(users[0].email, users[0].password); 
+    cy.request("DELETE",`https://staging.lpitko.ru/api/box/${boxKey}`);
   });
 });
